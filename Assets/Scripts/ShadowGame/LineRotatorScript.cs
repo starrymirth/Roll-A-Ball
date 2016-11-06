54d48a5b0aa68f2c115c352bd2341ca38e6cfe73
Laura <starrymirth@gmail.com>
2016-11-06 13:33:00 +0200

    Added win condition to shadow game, as well as a moves counter. (Closes #13)

--- Assets/Scripts/ShadowGame/LineRotatorScript.cs
+++ Assets/Scripts/ShadowGame/LineRotatorScript.cs
@@ -3,9 +3,11 @@
 public class LineRotatorScript : MonoBehaviour
 {
     public Vector3 Rotation;
+    public ShadowGameScript shadowScript; 
 
     void OnMouseDown()
     {
         transform.Rotate(Rotation);
+        shadowScript.IncrementCount();
     }
 }
---
+++ Assets/Scripts/ShadowGame/ShadowGameScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadowGameScript : MonoBehaviour
+{
+    private static int count;
+    public Text countText;
+    public Text winText;
+    public GameObject[] Lines;
+
+    void Start()
+    {
+        count = 0;
+        setCountText();
+        winText.text = "";
+    }
+
+    void Update()
+    {
+        bool won = true;
+        foreach (var line in Lines)
+        {
+            Vector3 angles = line.gameObject.transform.rotation.eulerAngles;
+            bool isvalid = Mathf.Abs(angles.x % 180) < 0.01
+                        && Mathf.Abs(angles.y % 180) < 0.01
+                        && Mathf.Abs(angles.z % 180) < 0.01;
+            won = won && isvalid;
+        }
+        if (won)
+        {
+            winText.text = "You Win!";
+            if (count == 10)
+            {
+                winText.text += "*";
+            }
+        }
+    }
+
+
+    public void IncrementCount()
+    {
+        count += 1;
+        setCountText();
+    }
+
+    void setCountText()
+    {
+        countText.text = string.Format("Score: {0}", count);
+    }
+}
