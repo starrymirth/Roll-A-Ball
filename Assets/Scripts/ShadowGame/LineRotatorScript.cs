be09f7f32696c9631c6d9f8118f31e10ba0d6aed
Laura <starrymirth@gmail.com>
2016-11-06 14:28:03 +0200

    Added sound to rotating puzzle piece (#11) as well as background music (fix #8) for shadowgame

--- Assets/Scripts/ShadowGame/LineRotatorScript.cs
+++ Assets/Scripts/ShadowGame/LineRotatorScript.cs
@@ -3,11 +3,24 @@
 public class LineRotatorScript : MonoBehaviour
 {
     public Vector3 Rotation;
-    public ShadowGameScript shadowScript; 
+    public ShadowGameScript shadowScript;
+    public AudioClip rotateSound;
+
+    private AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        rotateSound.LoadAudioData();
+    }
 
     void OnMouseDown()
     {
+        float volume = Random.Range(0.1f, 0.3f);
+        source.PlayOneShot(rotateSound, volume);
+
         transform.Rotate(Rotation);
         shadowScript.IncrementCount();
+        
     }
 }
--- Assets/Scripts/ShadowGame/ShadowGameScript.cs
+++ Assets/Scripts/ShadowGame/ShadowGameScript.cs
@@ -36,7 +36,6 @@ public class ShadowGameScript : MonoBehaviour
         }
     }
 
-
     public void IncrementCount()
     {
         count += 1;
