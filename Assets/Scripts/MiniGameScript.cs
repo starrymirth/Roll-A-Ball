202bd62d740264f87f6afd9fcf03fd7fb73df3ad
Laura <starrymirth@gmail.com>
2016-11-06 08:13:04 +0200

    Added a pause menu and allowed for pausing the game (Closed #1 and closed #3)

--- Assets/Scripts/MiniGameScript.cs
+++ Assets/Scripts/MiniGameScript.cs
@@ -2,23 +2,64 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class MiniGameScript : MonoBehaviour {
-    
-    // Use this for initialization
-    void Start () {
+public class MiniGameScript : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject PauseMenuCanvas;
 
+    void Start()
+    {
+        isPaused = !(Time.timeScale == 1);
+        PauseMenuCanvas.SetActive(isPaused);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene(1);
+            TogglePause();
         }
+    }
+
+    void TogglePause()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        PauseMenuCanvas.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        PauseMenuCanvas.SetActive(false);
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+        ResumeGame();
+    }
+    
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(1);
     }
 }
